fd59d9d25c34e9036d8643625c2a3bf82d9273d9
Wiem Mimouni <wiemmimouni74@gmail.com>
2023-09-24 21:10:37 +0100

    fix

--- alphabetinventory/solution.js
+++ alphabetinventory/solution.js
@@ -24,6 +24,7 @@ const alphaCount = (alphabet, text) => {
     results.sort();
   
     var resultString = results.join(',');
+    var resultString = results.join(',');
   
     return resultString;
   };
