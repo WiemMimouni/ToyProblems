6842642da4814b79016cf118023e9f5867721c76
Wiem Mimouni <wiemmimouni74@gmail.com>
2023-09-24 21:14:53 +0100

    done

--- coinSum/solution.js
+++ coinSum/solution.js
@@ -11,3 +11,29 @@ var makeChange = function(total) {
   
     return ways[total];
   };
+var makeChange = function(total) {
+    const coins = [1, 2, 5, 10, 20, 50, 100, 200];
+    const ways = new Array(total + 1).fill(0);
+    ways[0] = 1;
+  
+    for (let coin of coins) {
+      for (let i = coin; i <= total; i++) {
+        ways[i] += ways[i - coin];
+      }
+    }
+  
+    return ways[total];
+  };
+var makeChange = function(total) {
+    const coins = [1, 2, 5, 10, 20, 50, 100, 200];
+    const ways = new Array(total + 1).fill(0);
+    ways[0] = 1;
+  
+    for (let coin of coins) {
+      for (let i = coin; i <= total; i++) {
+        ways[i] += ways[i - coin];
+      }
+    }
+  
+    return ways[total];
+  };
